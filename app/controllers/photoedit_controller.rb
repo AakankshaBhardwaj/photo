0e6e982d50d33942c73957e9ae5c2708111248bf
aakanksha@gmail.com <bhardwaj.aakanksha@gmail.com>
2015-02-27 16:49:35 +0530

    edit pic

---
+++ app/controllers/photoedit_controller.rb
@@ -0,0 +1,2 @@
+class PhotoeditController < ApplicationController
+end
---
+++ test/controllers/photoedit_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PhotoeditControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
